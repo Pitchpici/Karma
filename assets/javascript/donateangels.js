45350b3454b87ca357d96f27d8572d7f128be3b1
Roxana Milea <roxanamilea@ymail.com>
2018-07-28 05:11:54 -0400

    fourth commit - user.uid

--- assets/javascript/donateangels.js
+++ assets/javascript/donateangels.js
@@ -20,6 +20,18 @@ $(document).ready(function() {
     var user = firebase.auth().currentUser;
 
     console.log("This is the current user: " + user);
+
+    var name, email, photoUrl, uid, emailVerified;
+
+      if (user != null) {
+        name = user.displayName;
+        email = user.email;
+        photoUrl = user.photoURL;
+        emailVerified = user.emailVerified;
+        uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+                         // this value to authenticate with your backend server, if
+                         // you have one. Use User.getToken() instead.
+      }
   	// var email = "clarkwmcd@gmail.com";
 
 
